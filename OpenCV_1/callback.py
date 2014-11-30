9fce1df7ff09205a6397b8d4e34e02e969083925
tristan02 <tristan.nico@gmail.com>
2014-11-30 18:44:27 +0100

    call back modificado
    Ahora con un click puedes capturar una imagen.
    Debería profundizar más con las funciones de opencv para video?
    Unas pocas dudas:
    1-Para que sirve el parametro param???
    2-Tampoco entiendo porque hay parametros que en las llamadas se igualan
    a otros o a un valor...
    3-Porque se pone un if delante del main
    
    Me puedes proponer una buena practica para esta semana? En plan para que
    mezcle los conocimientos adquiridos hasta ahora. Gracias

--- OpenCV_1/callback.py
+++ OpenCV_1/callback.py
@@ -6,23 +6,29 @@ class CallBack:
     def __init__(self):
         cv2.namedWindow("Camera", cv2.CV_WINDOW_AUTOSIZE )
         self.capture = cv2.VideoCapture(0)
-        self.param = "hola, son las "+time.asctime()
 
     def on_mouse(self,event, x, y, flag, param):
-        if(event == cv2.cv.CV_EVENT_MOUSEMOVE):
-           print param
-           print x,y
-           print flag
+        if(event == cv2.cv.CV_EVENT_FLAG_LBUTTON):
+            flag, src = self.capture.read()
+            img = cv2.imshow("Foto normal", src)
+                
 
     def callback(self):
         while True:
              flag, src = self.capture.read()
-             cv2.setMouseCallback("Camera",self.on_mouse, param = self.param)
+             cv2.setMouseCallback("Camera",self.on_mouse, param = 0)
              cv2.imshow("Camera", src)
-             self.param = "hola, son las "+time.asctime()
              if cv2.waitKey(10) == 27:
                   break
 
 if __name__ == '__main__':
     cb = CallBack()
     cb.callback()
+
+    cv2.destroyAllWindows()
+    
+'''
+1-Para que sirve el parametro param??? 
+2-Tampoco entiendo porque hay parametros que en las llamadas se igualan a otros o a un valor...
+3-Porque se pone un if delante del main
+'''
