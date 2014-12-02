bd3858a9d9f39777f5dd675a85c0ffc6008d7d11
tristan02 <tristan.nico@gmail.com>
2014-12-02 13:03:01 +0100

    Reestructuración del repositorio
    He añadido un package para separar todo lo que he hecho hasta ahora y el
    propio proyecto que quiero empezar ya

---
+++ OpenCV_1/Entrenamiento/canny.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+u1,u2=100,200
+
+def nothing():
+    cv2.imshow("Canny",edges)
+
+img = cv2.imread('zeb.jpg',0)
+cv2.namedWindow("umbrales",cv2.WINDOW_AUTOSIZE)
+cv2.createTrackbar("Umbral1", "umbrales", 0, 500, nothing)
+cv2.createTrackbar("Umbral2", "umbrales", 0, 500, nothing)
+
+
+while(1):
+    edges = cv2.Canny(img,u1,u2)
+    cv2.imshow("Original",img)
+    cv2.imshow("Canny",edges)
+    
+    u1 = cv2.getTrackbarPos("Umbral1", "umbrales")
+    u2 = cv2.getTrackbarPos("Umbral2", "umbrales")
+    
+    if cv2.waitKey(1) == 27:
+        cv2.imwrite("canny.jpg", edges)
+        break
+
+cv2.destroyAllWindows()
