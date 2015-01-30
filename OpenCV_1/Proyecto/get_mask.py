19107844e777905acdb1cf406e219437c27980d5
tristan02 <tristan.nico@gmail.com>
2015-01-30 15:54:14 +0100

    Seleccion de contorno deseado
    Solo sale en el 43% de los casos. =( Tengo que buscar otra solucion para
    filtrar los contornos y despues sacar las mascaras.

--- OpenCV_1/Proyecto/get_mask.py
+++ OpenCV_1/Proyecto/get_mask.py
@@ -22,8 +22,28 @@ def get_mask(img):
     '''cv2.imshow("thresh",thresh)
     cv2.waitKey()'''
     
+    #Seleccionamos el contorno que nos interesa
     contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(gray, contours, -1, (0,255,0), 3)    
+    max1 = 0
+    i = 0
+    for c in contours:
+        if cv2.arcLength(c, True) > max1:
+            max1 = cv2.arcLength(c, True)
+            cnt = contours[i]
+        i = i + 1    
+    i = 0
+    max2 = 0
+    for c in contours:
+        if cv2.arcLength(c, True) > max2 and cv2.arcLength(c, True) != max1:
+            max2 = cv2.arcLength(c, True)
+            cnt = contours[i]
+        i = i + 1     
+    archi=open('datos.txt','a')
+    for line in cnt:        
+        archi.write(str(line) + '\n')
+    archi.close()   
+    cv2.drawContours(gray, [cnt], 0, (0,255,0), 3)            
+    #cv2.drawContours(gray, contours, i, (0,255,0), 3)    
     '''cv2.imshow('contours', gray)
     cv2.waitKey()'''
     
